9ca00adb314c3872d6a155c51e88a7fe809bd3aa
gangsoooha <happy95virus@gmail.com>
2023-12-16 20:21:13 +0900

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,22 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { VertexNormalsHelper } from 'three/examples/jsm/helpers/VertexNormalsHelper.js';
-import vertexShader from '../shaders/earth/vertex.glsl?raw';
-import fragmentShader from '../shaders/earth/fragment.glsl?raw';
-import pointsVertexShader from '../shaders/earthPoints/vertex.glsl?raw';
-import pointsFragmentShader from '../shaders/earthPoints/fragment.glsl?raw';
-import glowVertexShader from '../shaders/earthGlow/vertex.glsl?raw';
-import glowFragmentShader from '../shaders/earthGlow/fragment.glsl?raw';
-import CameraControls from '../Controls/CameraControls';
-import Animation from '../Controls/Animation';
+import { texutreURL  } from "./texture.js"
 import GUI from 'lil-gui';
 import { gsap } from 'gsap';
+
+
+
 
 export default function () {
   const renderer = new THREE.WebGLRenderer({
     alpha: true,
-  });
+  }
+  );
   renderer.setClearColor(0x000000, 1);
+  renderer.shadowMap.enabled = true;
+
 
   const container = document.querySelector('#container');
-
   container.appendChild(renderer.domElement);
 
   const canvasSize = {
@@ -27,168 +24,140 @@ export default function () {
     height: window.innerHeight,
   };
 
+
+  
+
   const clock = new THREE.Clock();
-  const textureLoader = new THREE.TextureLoader();
+  // const textureLoader = new THREE.TextureLoader();
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
-    75,
+    45,
     canvasSize.width / canvasSize.height,
-    0.1,
+    1,
     100
   );
-
-  /** BackGround 
-  const cubeTextureLoader = new THREE.CubeTextureLoader();
-  const environmentMap = cubeTextureLoader.load([
-   'assets/environment/px.png',
-   'assets/environment/nx.png',
-   'assets/environment/py.png',
-   'assets/environment/ny.png',
-   'assets/environment/pz.png',
-   'assets/environment/nz.png'
-  ])
-  environmentMap.encoding = THREE.sRGBEncoding;
-  scene.background = environmentMap;
-  scene.environment = environmentMap;
-*/
 
   /** library */
   //const gui = new GUI();
   
-  /** animation */
-  let introStart = true;
-  const animation = new Animation(introStart,gsap);
-  
-  //introStart = animation.animationIntro();
-  
+  /** light */
+  function getSpotLight(color, intensity) {
+    var light = new THREE.SpotLight(color, intensity);
+    light.castShadow = true;
 
+    light.shadow.mapSize.x = 4096;
+    light.shadow.mapSize.y = 4096;
 
-  /** Camera */
-  //camera.position.set(0, 0, 1.9);
-  camera.position.set(0, 0, 0.85);
-
-  /** Controls */
-  const cameraControls = new CameraControls();
-  const orbitControls = () => {
-    const controls = new OrbitControls(camera, renderer.domElement);
-
-    return controls;
+    return light;
   }
   
-  /** create Earth */
-  const createEarth = () => {
-    const material = new THREE.ShaderMaterial({
-      wireframe: false,
-      uniforms: {
-        uTexture: {
-          value: textureLoader.load('assets/earth-specular-map.png'),
-        },
-      },
-      vertexShader: vertexShader,
-      fragmentShader: fragmentShader,
-      side: THREE.DoubleSide,
-      transparent: true,
-    });
-
-    const geometry = new THREE.SphereGeometry(0.8, 30, 30);
-    const mesh = new THREE.Mesh(geometry, material);
-
-    return mesh;
-  };
-
-  /** create EarthPointe*/
-  const createEarthPoints = () => {
-    const material = new THREE.ShaderMaterial({
-      wireframe: true,
-      uniforms: {
-        uTexture: {
-          value: textureLoader.load('assets/earth-specular-map.png'),
-        },
-        uTime: {
-          value: 0,
-        },
-      },
-      vertexShader: pointsVertexShader,
-      fragmentShader: pointsFragmentShader,
-      side: THREE.DoubleSide,
-      transparent: true,
-      depthWrite: false,
-      depthTest: false,
-      blending: THREE.AdditiveBlending,
-    });
-
-    const geometry = new THREE.IcosahedronGeometry(0.8, 30, 30);
-    geometry.rotateY(-Math.PI);
-
-    const mesh = new THREE.Points(geometry, material);
-
-    return mesh;
-  };
-
+  var spotLight_01 = getSpotLight('rgb(145,200,255)', 1);
+  var spotLight_02 = getSpotLight('rgb(255,220,180)', 1);
+  scene.add(spotLight_01);
+  scene.add(spotLight_02);
   
-  /** create EarthGlow */
-  const createEarthGlow = () => {
-    const material = new THREE.ShaderMaterial({
-      uniforms: {
-        uZoom: {
-          value: 1,
-        },
-      },
-      vertexShader: glowVertexShader,
-      fragmentShader: glowFragmentShader,
-      side: THREE.BackSide,
-      transparent: true,
+  /** Camera */
+  camera.position.x = 4;
+  camera.position.y = 8;
+  camera.position.z = 11;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  spotLight_01.position.x = 6;
+	spotLight_01.position.y = 8;
+	spotLight_01.position.z = -20;
+  
+	spotLight_02.position.x = -12;
+	spotLight_02.position.y = 6;
+	spotLight_02.position.z = -10;
+
+
+
+  /** Controls */
+  const orbitControls = () => {
+    const controls = new OrbitControls(camera, renderer.domElement);
+    return controls;
+  }
+
+  /** texture */
+  function getTexture(obj) {
+    var url = texutreURL(obj)
+    return url
+  }
+
+
+  var textureLoader = new THREE.TextureLoader();
+  // var texture = textureLoader.load('public/assets/road1.avif');
+  var texture = textureLoader.load(getTexture('plane'));
+  var metalTexture = textureLoader.load(getTexture('shape'));
+  var repetition = 6;
+  var textures = ['map','bumpMap','roughnessMap']
+
+
+  /** createObject */
+  const createBall = (radius) => {
+    var geometry = new THREE.SphereGeometry(radius, 24, 24);
+    var material = new THREE.MeshStandardMaterial({
+      color: 'gray',
+      bumpMap : metalTexture,
+      roughnessMap : metalTexture,
+    });
+    var sphere = new THREE.Mesh(geometry, material);
+    var sphereMaterial = sphere.material;
+
+    sphere.castShadow = true;
+    sphere.position.y = sphere.geometry.parameters.radius;
+    
+    sphereMaterial.bumpScale = 0.01;
+    sphereMaterial.roughness = 0.75;
+    sphereMaterial.metalness = 0.25;
+
+    return sphere;
+  }
+
+
+
+  const creatPlane = (w,h) => {
+    var geo = new THREE.PlaneGeometry(w, h);
+    var material = new THREE.MeshStandardMaterial({
+      color: 0xffffff,
+      side: THREE.DoubleSide,
+      map: texture,
+      bumpMap : texture,
+      roughnessMap : texture,
+    });
+    
+    var mesh = new THREE.Mesh(geo, material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    
+    mesh.rotation.x = Math.PI/2;
+
+    const planeMaterial = mesh.material;
+    planeMaterial.roughness = 0.65;
+    planeMaterial.metalness = 0.75;
+    planeMaterial.bumpScale = 0.01
+
+    textures.forEach((mapName) => {
+      planeMaterial[mapName].wrapS = THREE.RepeatWrapping;
+      planeMaterial[mapName].wrapT = THREE.RepeatWrapping;
+      planeMaterial[mapName].repeat.set(repetition, repetition); 
     });
 
-    const geometry = new THREE.SphereGeometry(1, 40, 40);
-    const mesh = new THREE.Mesh(geometry, material);
+    
+
 
     return mesh;
-  };
-
-  /** create Galaxis */
-  const createGalaxis = () => {
-    const count  = 10000;
-    const positions = new Float32Array(count * 3);
-    for (let i = 0; i < count; i++){
-      positions[i] = (Math.random() - 0.5) * 4;
-      positions[i + 1] = (Math.random() - 0.5) * 4;
-      positions[i + 2] = (Math.random() - 0.5) * 4; 
-    }
-    const starsGeometric = new THREE.BufferGeometry();
-    starsGeometric.setAttribute(
-      'position',
-      new THREE.BufferAttribute(positions, 3)
-    )
-    const starsMaterial = new THREE.PointsMaterial({
-      size : Math.random() * 0.007,
-      transparent : true,
-      depthWrite : false,
-      color: '#3f9f8e',
-      alphaMap : textureLoader.load('assets/particle.png'),
-      map: textureLoader.load('assets/particle.png'),
-    })
-    const star = new THREE.Points(starsGeometric, starsMaterial);
-
-    return star
-  }  
-
+  
+  }
 
   /** create */
   const create = () => {
-    const earth = createEarth();
-    const earthPoints = createEarthPoints();
-    const earthGlow = createEarthGlow();
-    const stars = createGalaxis()
-    // const glowNormalHelper = new VertexNormalsHelper(earthGlow, 0.1);
-
-    scene.add(earth, earthPoints, earthGlow, stars);
-
+    var sphere = createBall(1);
+    var plane = creatPlane(50, 50);
+    scene.add(sphere, plane);
     return {
-      earth,
-      earthPoints,
-      earthGlow,
-      stars
+      sphere,
+      plane
     };
   };
 
@@ -203,21 +172,15 @@ export default function () {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   };
 
-  let lastScrollY = 0;
-  let entered = false;
+
+
   const addEvent = () => {
     window.addEventListener('resize', resize);
     window.addEventListener("scroll", () => {
-      lastScrollY = animation.scrollAnimation(window.scrollY,lastScrollY);
-      animation.enterToContent(camera);
     });
     window.addEventListener('mousedown', (e) => {
-      console.log('d '+e.clientX)
-      console.log('d ' +e.clientY)
     })
     window.addEventListener('mouseup', (e) => {
-      console.log('u '+e.clientX)
-      console.log('u '+e.clientY)
     })
   
   };
@@ -225,27 +188,21 @@ export default function () {
 
 
   const draw = (obj, orbitControl) => {
-    const { earth, earthPoints, earthGlow, stars} = obj;
-    // earth.rotation.x += 0.0005;
-    earth.rotation.y += 0.0005;
-    earth.rotation.x = 0.3;
-    // earthPoints.rotation.x += 0.0005;
-    earthPoints.rotation.y += 0.0005;
-    earthPoints.rotation.x = 0.3;
-    // stars.rotation.x += 0.0007;
-     stars.rotation.y += 0.0007;
+    const { sphere, plane } = obj;
+
+    spotLight_01.intensity += (Math.random() - 0.5) * 0.15;
+    spotLight_01.intensity = Math.abs(spotLight_01.intensity);
+
+    spotLight_02.intensity += (Math.random() - 0.5) * 0.05;
+    spotLight_02.intensity = Math.abs(spotLight_02.intensity);
 
 
+    // sphere.scale.x += 0.01;
+    // sphere.scale.z += 0.01;
 
 
     orbitControl.update();
     renderer.render(scene, camera);
-    // console.log(orbitControl.getDistance)
-    earthGlow.material.uniforms.uZoom.value = orbitControl.target.distanceTo(
-      orbitControl.object.position
-    );
-
-    earthPoints.material.uniforms.uTime.value = clock.getElapsedTime();
 
     requestAnimationFrame(() => {
       draw(obj, orbitControl);
